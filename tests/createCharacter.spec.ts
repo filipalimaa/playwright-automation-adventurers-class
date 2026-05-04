a208a1df001f4a1994ce0900ce14d3db1e89e604
Filipa Lima <filipalima1995@gmail.com>
2026-05-04 16:25:21 +0100

    feat: add some tests to the character creation and getting the character Id

--- tests/createCharacter.spec.ts
+++ tests/createCharacter.spec.ts
@@ -1,6 +1,6 @@
 import { createCharacter, getMyCharacter } from "./client/character-client";
 import { getToken } from "./client/token-client";
-import { request, test } from '@playwright/test'
+import { expect, request, test } from '@playwright/test'
 /* import { BARBARIAN_CHAR, WIZARD_CHAR } from "./data/create-character-data";*/
 import { WARLOCK_CHAR } from "./data/create-character-data";
 
@@ -13,13 +13,18 @@ test.describe.serial('Create Bruno The Barbarian', () => {
         token = await getToken(request);
     });
 
-    test('Create My Warlock Character', async ({request}) => {
+    test('Create My Warlock Character', async ({ request }) => {
 
         const characterResponse = await createCharacter(
             request, 
             token, 
             WARLOCK_CHAR,
         );
+
+        expect(characterResponse.status()).toBe(201);
+
+        const responseBody = await characterResponse.json();
+        expect(responseBody.id).toBeDefined();
 
     });
 
@@ -32,6 +37,17 @@ test.describe.serial('Create Bruno The Barbarian', () => {
         );
 
     });*/
+
+    test('Get All Characters and find my Warlock Character by Name', async ({ request }) => {
+
+        const myCharacter = await getMyCharacter(request, token);
+
+        expect(myCharacter).toBeDefined();
+        expect(myCharacter.name).toBe('Vael Phira, The Warlock Half-elf');
+
+        characterId = myCharacter.id;
+        expect(characterId).toBeGreaterThan(0);
+    })
 
 
 
